da4dddd96fa079ffbb44d7ae5f174043f56fa9ae
Sumon Ahmed <sumoncse15@gmail.com>
2020-08-28 18:08:36 +0600

    Initial Commit

---
+++ src/com/example/addinglowertypes/AddingLowerTypes.java
@@ -0,0 +1,14 @@
+package com.example.addinglowertypes;
+
+public class AddingLowerTypes {
+
+    public static void main(String[] args) {
+
+        byte a=30;
+        byte b=70;
+
+        byte c= (byte)(a+b);
+        System.out.println(c);
+
+    }
+}
